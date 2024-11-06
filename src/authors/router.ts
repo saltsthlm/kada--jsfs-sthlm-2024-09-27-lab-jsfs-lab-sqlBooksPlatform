7a2777b3504a5db6cd645f91b11237a5ef9c8e4f
Anton Berglund <antonberglund_92@hotmail.com>
2024-11-06 14:03:07 +0100

    create-authors-router
    Co-authored-by: Leo <leo@swederus.se>
    Co-authored-by: Carl <carl@wbsweden.com>

---
+++ src/authors/router.ts
@@ -0,0 +1,13 @@
+import express from 'express';
+import { AuthorService } from './service';
+
+export const createAuthorsRouter = (service: AuthorService) => {
+        const router = express.Router()
+
+        router.get('/', async (req, res) => {
+            const authors = await service.getAll();
+            res.status(200).json(authors);
+        })
+
+        return router;
+}
--- src/authors/service.ts
+++ src/authors/service.ts
@@ -1,11 +1,11 @@
 import { PoolClient } from "pg";
 
-export const createAuthorService = async (db: PoolClient) => {
+export const createAuthorService = (db: PoolClient) => {
   return {
     async getAll() {
       const query = "SELECT * FROM authors";
       const authors = await db.query(query);
-      return authors.rows[0];
+      return authors.rows;
     },
     async getById(id: string) {
       const query = "SELECT * FROM authors WHERE id = $1";
@@ -14,3 +14,5 @@ export const createAuthorService = async (db: PoolClient) => {
     },
   };
 };
+
+export type AuthorService = ReturnType<typeof createAuthorService>;
