1ed92a8d2648e38d47525965b045844f1c294d47
Leo Swedérus <leo.swederus@appliedtechnology.se>
2024-11-06 15:18:50 +0100

    made-post-and-patch
    Co-authored-by: Anton <antonberglund_92@hotmail.com>
    Co-authored-by: Carl <carl@wbsweden.com>

---
+++ src/books/service.ts
@@ -0,0 +1,41 @@
+import { describe } from "node:test";
+import { PoolClient } from "pg";
+import { z, ZodSchema } from "zod"
+
+export const createBookService = (db: PoolClient) => {
+  return {
+    async getAll() {
+      const query = "SELECT * FROM books";
+      const books = await db.query(query);
+      return books.rows;
+    },
+    async getById(id: string) {
+      const query = "SELECT * FROM books WHERE id=$1";
+      const book = await db.query(query, [id]);
+      if (book.rows.length === 0) {
+        throw new Error("Book with that id does not exist");
+      }
+      return book.rows[0];
+    },
+    async post(book: Book) {
+        const {title, description, price, author_id } = book
+        bookSchema.parse(book)
+        const query = "INSERT INTO books (title, description, price, author_id) values($1, $2, $3, $4)"
+        await db.query(query, [title, description, price, author_id])
+    }
+    async patch(book: Book) {
+        
+    }
+  };
+};
+
+export const bookSchema = z.object({
+    id: z.string(),
+    title: z.string(),
+    description: z.string(),
+    price: z.number(),
+    author_id: z.string()
+});
+type Book = z.infer<typeof bookSchema>;
+
+export type BookService = ReturnType<typeof createBookService>;
