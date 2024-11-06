fe3e8e073b3d467fe464cdaaa614e337f84667d2
Anton Berglund <antonberglund_92@hotmail.com>
2024-11-06 13:47:03 +0100

    add-database-seed
    Co-authored-by: Leo <leo@swederus.se>
    Co-authored-by: Carl <carl@wbsweden.com>

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,23 @@
 import express, { Express } from 'express';
 
 import instructors from './instructors';
+import { createDatabase } from './db';
+import { createTablesAndSeedData } from './authors/db-seed';
+import { createAuthorService } from './authors/service';
 
 const app: Express = express();
 const port = 3001;
+
+(async() => {
+    const client = await createDatabase();
+    client.query(createTablesAndSeedData());
+
+    const service = await createAuthorService(client);
+    const authors = await service.getAll()
+    console.log(authors);
+    
+})();
+
 
 app.use('/api/instructors', instructors.router);
 
