ea08b5efabb694c9bad8d041bcf71af8b71e6c08
Anton Berglund <antonberglund_92@hotmail.com>
2024-11-07 13:10:53 +0100

    added-drizzle-to-project
    Co-authored-by: Leo <leo@swederus.se>
    Co-authored-by: Carl <carl@wbsweden.com>

--- src/app.ts
+++ src/app.ts
@@ -5,19 +5,37 @@ import { createAuthorFeature } from "./authors/feature";
 import { createBookFeature } from "./books/feature";
 import { createPurchasesFeature } from "./purchases/feature";
 import { createBuyersFeature } from "./buyers/feature";
+import 'dotenv/config';
+import { drizzle } from 'drizzle-orm/node-postgres';
+import { authorsTable } from "./authors/schema";
 
 const app: Express = express();
 const port = 3001;
 
 (async () => {
-  const client = await createDatabase();
-  client.query(createTablesAndSeedData());
+
+  const db = drizzle(process.env.DATABASE_URL!);
+  const user: typeof authorsTable.$inferInsert = {
+    name: 'John',
+    bio: 'detta är en bio',
+  };  
+
+  await db.insert(authorsTable).values(user);
+  console.log('New user created!')
+
+  const users = await db.select().from(authorsTable);
+  console.log('Getting all users from the database: ', users)
+  //const client = await createDatabase();
+  //client.query(createTablesAndSeedData());
+
+  //const books = await db.select().from('books');
+  //console.log(books);
 
   app.use(express.json());
-  app.use("/api/authors", createAuthorFeature(client));
+/*   app.use("/api/authors", createAuthorFeature(client));
   app.use("/api/books", createBookFeature(client));
   app.use("/api/buyers", createBuyersFeature(client));
-  app.use("/api/purchases", createPurchasesFeature(client));
+  app.use("/api/purchases", createPurchasesFeature(client)); */
 })();
 
 app.listen(port, () =>
---
+++ src/authors/schema.ts
@@ -0,0 +1,7 @@
+import { integer, pgTable, varchar } from "drizzle-orm/pg-core";
+
+export const authorsTable = pgTable("authors", {
+  id: integer().primaryKey().generatedAlwaysAsIdentity(),
+  name: varchar({ length: 120 }).notNull(),
+  bio: varchar({ length: 250 }).notNull()
+});
