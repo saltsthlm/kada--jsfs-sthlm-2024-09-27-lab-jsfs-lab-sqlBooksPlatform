c4941af49ae47701b89f4c578c15a12e29aedb78
Leo Swedérus <leo.swederus@appliedtechnology.se>
2024-11-07 14:44:17 +0100

    fixed-code
    Co-authored-by: Anton <antonberglund_92@hotmail.com>
    Co-authored-by: Carl <carl@wbsweden.com>

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -2,7 +2,7 @@ import "dotenv/config";
 import { defineConfig } from "drizzle-kit";
 export default defineConfig({
   out: "./drizzle",
-  schema: "./src/authors/schema.ts",
+  schema: "./src/feature/**/schema.ts",
   dialect: "postgresql",
   dbCredentials: {
     url: process.env.DATABASE_URL!,
--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,9 @@ import express, { Express } from "express";
 import { createAuthorFeature } from "./feature/authors/feature";
 import "dotenv/config";
 import { drizzle } from "drizzle-orm/node-postgres";
+import { createBookFeature } from "./feature/books/feature";
+import { createBuyersFeature } from "./feature/buyers/feature";
+import { createPurchasesFeature } from "./feature/purchases/feature";
 
 
 const app: Express = express();
@@ -12,9 +15,9 @@ const db = drizzle(process.env.DATABASE_URL!);
 (async () => {
   app.use(express.json());
   app.use("/api/authors", createAuthorFeature(db).router);
-  // app.use("/api/books", createBookFeature(client));
-  // app.use("/api/buyers", createBuyersFeature(client));
-  // app.use("/api/purchases", createPurchasesFeature(client));
+  app.use("/api/books", createBookFeature(db).router);
+  app.use("/api/buyers", createBuyersFeature(db).router);
+  app.use("/api/purchases", createPurchasesFeature(db).router);
 })();
 
 app.listen(port, () =>
