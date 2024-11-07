0bdd8f51ce49663342722cc33b58af79293f9ee2
Leo Swedérus <leo.swederus@appliedtechnology.se>
2024-11-07 14:36:34 +0100

    made-schema-for-books
    Co-authored-by: Anton <antonberglund_92@hotmail.com>
    Co-authored-by: Carl <carl@wbsweden.com>

---
+++ src/feature/books/schema.ts
@@ -0,0 +1,9 @@
+import { integer, pgTable, varchar } from "drizzle-orm/pg-core";
+
+export const booksTable = pgTable("books", {
+  id: integer().primaryKey().generatedAlwaysAsIdentity(),
+  title: varchar({ length: 120 }).notNull(),
+  description: varchar({ length: 250 }).notNull(),
+  price: integer().notNull(),
+  author_id: integer().notNull()
+});
