2cffbad8c6d6dba7663c1ec2c92a0fc16318d6db
Leo Swedérus <leo.swederus@appliedtechnology.se>
2024-11-07 16:01:03 +0100

    refracture-code
    Co-authored-by: Anton <antonberglund_92@hotmail.com>
    Co-authored-by: Carl <carl@wbsweden.com>

--- src/feature/books/router.ts
+++ src/feature/books/router.ts
@@ -12,16 +12,16 @@ export const createRouter = (service: BookService) => {
     res.status(200).json(book);
   });
   router.post("/", async (req, res) => {
-    const result = await service.add(req.body);
+    await service.add(req.body);
     res.status(201).json();
   });
   router.patch("/:id", async (req, res) => {
     const book = await service.patch(req.body, req.params.id);
     res.status(200).json(book);
   });
-/*   router.delete("/:id", async (req, res) => {
+    router.delete("/:id", async (req, res) => {
     await service.delete(req.params.id);
     res.status(204).json();
-  }); */
+  });
   return router;
 };
