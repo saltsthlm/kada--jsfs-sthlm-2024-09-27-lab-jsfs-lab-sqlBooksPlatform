077ff24a2f835a4bfdb92885b9075cbb1df85d12
Leo Swedérus <leo.swederus@appliedtechnology.se>
2024-11-07 14:44:00 +0100

    made-schmea-for-purchases
    Co-authored-by: Anton <antonberglund_92@hotmail.com>
    Co-authored-by: Carl <carl@wbsweden.com>

---
+++ src/feature/purchases/schema.ts
@@ -0,0 +1,7 @@
+import { integer, pgTable, timestamp} from "drizzle-orm/pg-core";
+
+export const purchasesTable = pgTable("purchases", {
+  buyer_id: integer().primaryKey().generatedAlwaysAsIdentity(),
+  book_id: integer().notNull(),
+  purchase_date: timestamp().notNull(),
+});
