c73f205154a152d9600bd6f9121bcadf387e7bd6
Anton Berglund <antonberglund_92@hotmail.com>
2024-11-08 11:34:21 +0100

    add-better-error-handle
    Co-authored-by: Leo <leo@swederus.se>
    Co-authored-by: Carl <carl@wbsweden.com>

--- src/middleware/error-handler.ts
+++ src/middleware/error-handler.ts
@@ -1,15 +1,22 @@
 import { ErrorRequestHandler } from "express";
 import { ZodError } from "zod";
+import { AppError } from "../errors";
 export function createErrorRequestHandler() {
-  const errorHandler: ErrorRequestHandler = (err, _req, res, next) => {
+  const errorHandler: ErrorRequestHandler = (err, _req, res, _next) => {
     if (err instanceof ZodError) {
       res.status(400).json(err.errors);
     }
-    if (err.message === "MMM") {
-      res.status(409).json("FEL IGEN?");
-    } 
-    else {
-      next(err);
+    if (err instanceof AppError) {
+      if (err.statusMessage() === "NOT_FOUND_ERROR") {
+        res.status(404).json(err.message);
+        return;
+      }
+      if (err.statusMessage() === "BAD_REQUEST_ERROR") {
+        res.status(500).json(err.message);
+        return;
+      }
+    } else {
+      res.status(500).json({ message: "Internal server error" });
     }
   };
   return errorHandler;
