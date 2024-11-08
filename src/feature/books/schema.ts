483670eec717b75ddc1e93ef48b49cc25cb8e1f5
CarlUdo <carl@wbsweden.com>
2024-11-08 13:15:55 +0100

    Change-schema-type
    Co-authored-by: Leo <leo@swederus.se>
    Co-authored-by: Anton <antonberglund_92@hotmail.com>

--- src/feature/books/schema.ts
+++ src/feature/books/schema.ts
@@ -1,9 +1,9 @@
-import { integer, pgTable, varchar } from "drizzle-orm/pg-core";
+import { integer, pgTable, varchar, numeric } from "drizzle-orm/pg-core";
 
 export const booksTable = pgTable("books", {
   id: integer().primaryKey().generatedAlwaysAsIdentity(),
   title: varchar({ length: 120 }).notNull(),
   description: varchar({ length: 250 }).notNull(),
-  price: integer().notNull(),
+  price: numeric().notNull(), // ÅTERSKAPA FEL SÅ ATT VI KAN FÅNGA DET!!!!
   author_id: integer().notNull(),
 });
