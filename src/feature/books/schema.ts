7f387d380212c86013d0c04678d5ba0c26fa01b8
CarlUdo <carl@wbsweden.com>
2024-11-08 16:35:04 +0100

    Update-book-schema
    Co-authored-by: Leo <leo@swederus.se>
    Co-authored-by: Anton <antonberglund_92@hotmail.com>

--- src/feature/books/schema.ts
+++ src/feature/books/schema.ts
@@ -1,9 +1,14 @@
-import { integer, pgTable, varchar, numeric } from "drizzle-orm/pg-core";
+import { integer, pgTable, varchar } from "drizzle-orm/pg-core";
+import { authorsTable } from "../authors/schema";
 
 export const booksTable = pgTable("books", {
   id: integer().primaryKey().generatedAlwaysAsIdentity(),
   title: varchar({ length: 120 }).notNull(),
   description: varchar({ length: 250 }).notNull(),
-  price: numeric().notNull(), // ÅTERSKAPA FEL SÅ ATT VI KAN FÅNGA DET!!!!
-  author_id: integer().notNull(),
+  price: varchar().notNull(), // ÅTERSKAPA FEL SÅ ATT VI KAN FÅNGA DET!!!!
+});
+
+export const booksToAuthors = pgTable("books_to_authors", {
+  bookId: integer("book_id").notNull().references(() => booksTable.id),
+  authorId: integer("author_id").notNull().references(() => authorsTable.id),
 });
